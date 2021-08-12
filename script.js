e93fee4c5ce361bde9a7d761e6dbdfaee9657e8a
raphabraga <raphael-braga@hotmail.com>
2021-08-12 16:33:57 -0300

    create function for refresh rendering each 100ms. implemented snake motion

--- script.js
+++ script.js
@@ -6,16 +6,48 @@ snake[0] = {
   x: 8 * box,
   y: 8 * box,
 };
+let direction = "right";
 
-createBackground = () => {
+const createBackground = () => {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
 };
 
-drawSnake = (snake) => {
+const drawSnake = () => {
   context.fillStyle = "green";
   snake.forEach((el) => context.fillRect(el.x, el.y, box, box));
 };
 
-createBackground();
-drawSnake(snake);
+const gameStart = () => {
+  createBackground();
+  drawSnake();
+
+  let { x: headX, y: headY } = snake[0];
+  console.log(headX, headY);
+  switch (direction) {
+    case "left":
+      headX -= box;
+      break;
+    case "right":
+      headX = headX === 16 * box ? 0 : headX + box;
+      break;
+    case "up":
+      headY -= box;
+      break;
+    case "down":
+      headY -= box;
+      break;
+    default:
+      break;
+  }
+  snake.pop();
+  snake.unshift({ x: headX, y: headY });
+};
+
+const refreshRender = () => {
+  window.setInterval(() => {
+    gameStart();
+  }, 100);
+};
+
+let gameRender = refreshRender();
